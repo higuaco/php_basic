9e0df569cb55309b531316b3476404b33a36e749
Higuaco <higuu28@gmail.com>
2021-08-14 00:38:45 +0900

    作成

---
+++ check.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎</title>
+  <link rel="icon" href="favicon.ico">
+  <meta name="viewport"
+    content="width=device-width,initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <?php
+  $nickname = $_POST['nickname'];
+  $email = $_POST['email'];
+  $goiken = $_POST['goiken'];
+
+  if($nickname=='') {
+    echo 'ニックネームが入力されていません。<br />';
+  }else{
+    echo 'ようこそ';
+    echo $nickname;
+    echo '様';
+    echo '<br />';
+  }
+  if($email==''){
+    echo 'メールアドレスが入力されていません。<br />';
+  }else{
+    echo 'メールアドレス：';
+    echo $email;
+    echo '<br />';
+  }
+  if($goiken==''){
+    echo 'ご意見が入力されていません。<br />';
+  }else{
+    echo 'ご意見『';
+    echo $goiken;
+    echo '』<br />';
+  }
+
+  if ($nickname==''|| $email==''|| $goiken=='') {
+      echo '<form action="">';
+      echo '<input type="button" onclick="history.back()" value="戻る">';
+      echo '</form>';
+  }else {
+    echo '<form method="post" action="thanks.php">';
+    echo '<input type="button" onclick="history.back()" value="戻る">';
+    echo '<input type="submit" value="戻る">';
+    echo '</form>'
+  }
+
+?>
+</body>
+
+</html>
---
+++ thanks.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎</title>
+  <link rel="icon" href="favicon.ico">
+  <meta name="viewport"
+    content="width=device-width,initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <?php
+  echo 'ご意見ありがとうございました。<br />';
+  ?>
+</body>
+
+</html>
